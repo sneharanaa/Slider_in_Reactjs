023f4f2c077ab7f7d38539a7aff25ab65d66d984
Sneha Rana <154796175+sneharanaa@users.noreply.github.com>
2024-08-04 16:39:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Slider from './components/Slider/Slider'
+
+
+function App() {
+  return (
+    <div className='bg-zinc-900 text-white align-center h-full w-100' >
+      <Slider autoSlide={true} /> 
+    </div>
+  )
+}
+
+export default App
